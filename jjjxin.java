1612753d17871a6545650fe234f71a66879c7382
JIANGjxin <37327089+JIANGjxin@users.noreply.github.com>
2018-03-27 10:17:44 +0800

    Add files via upload

---
+++ jjjxin.java
@@ -0,0 +1,13 @@
+/**
+ * Created by jjxin on 18-3-27.
+ */
+public class jjjxin {
+        public static void main(String[] args){
+            String s="zhangsan,18,192.168.36.22,8.8.8.8";
+            String[] m=s.split(",");
+            for(String i:m){
+                System.out.println(i);
+            }
+        }
+    }
+
